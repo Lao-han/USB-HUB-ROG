25699445669dee6f57de6d1c69832e2bc961eb52
Lao-han <99854153+Lao-han@users.noreply.github.com>
2024-04-17 18:00:42 +0800

    添加FreeRTOS

---
+++ Software/keil5/OBJ/heap_4.d
@@ -0,0 +1,14 @@
+..\obj\heap_4.o: ..\FreeRTOS\portable\MemMang\heap_4.c
+..\obj\heap_4.o: E:\Study\Study_app\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
+..\obj\heap_4.o: E:\Study\Study_app\Keil\ARM\ARMCC\Bin\..\include\string.h
+..\obj\heap_4.o: ..\FreeRTOS\include\FreeRTOS.h
+..\obj\heap_4.o: E:\Study\Study_app\Keil\ARM\ARMCC\Bin\..\include\stddef.h
+..\obj\heap_4.o: E:\Study\Study_app\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\heap_4.o: ..\FreeRTOS\include\FreeRTOSConfig.h
+..\obj\heap_4.o: ..\FreeRTOS\include\projdefs.h
+..\obj\heap_4.o: ..\FreeRTOS\include\portable.h
+..\obj\heap_4.o: ..\FreeRTOS\include\deprecated_definitions.h
+..\obj\heap_4.o: ..\FreeRTOS\portable\RVDS\ARM_CM3\portmacro.h
+..\obj\heap_4.o: ..\FreeRTOS\include\mpu_wrappers.h
+..\obj\heap_4.o: ..\FreeRTOS\include\task.h
+..\obj\heap_4.o: ..\FreeRTOS\include\list.h
